117a382d0ebe8f8e958b7cc86a01abc1315101f0
Hilixx <jhonalizan@gmail.com>
2024-09-06 14:07:08 -0300

    updateUser

--- src/main/java/com/example/springboot/controllers/UserController.java
+++ src/main/java/com/example/springboot/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.example.springboot.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,5 +39,17 @@ public class UserController {
 	public ResponseEntity<List<UserModel>> allUsers(){
 		return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());	
 	}
+	@PutMapping("/users/{id}")
+	public ResponseEntity<Object> userUpdate( @PathVariable(value = "id") UUID id, @RequestBody @Valid UserRecordDto userRecordDto){
+		Optional<UserModel> userO = userRepository.findById(id);
+		if(userO.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario não encontrado");
+			
+		}
+		var userModel = userO.get();
+		BeanUtils.copyProperties(userRecordDto,userModel);
+		return ResponseEntity.status(HttpStatus.OK).body(userRepository.save(userModel));
+	}
+	
 	
 }
